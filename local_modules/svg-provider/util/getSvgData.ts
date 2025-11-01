8dc0fac91a2d59aa9419afdcbb6b82a4867d0282
Ivan Molto <ivanmolto@gmail.com>
2025-11-01 19:54:42 +0100

    feat: Add getSvgData utility for extracting SVG content and attributes

---
+++ local_modules/svg-provider/util/getSvgData.ts
@@ -0,0 +1,42 @@
+import replaceSvgIds from "./replaceSvgIds";
+
+type SvgData = {
+  body: string;
+  attributes: Record<string, string>;
+};
+
+const getSvgData = (html: string): SvgData | undefined => {
+  try {
+    const svgText = html.replace(/<\?xml(.*?)\?>/, "");
+    const svgTag =
+      svgText.match(/<(svg|symbol)[^>]+>/)?.[0].replace(/[\n\t]/g, " ") || "";
+
+    let body = svgText
+      .replace("</svg>", "")
+      .replace(/<svg[^>]+>/, "")
+      .trim();
+
+    body = replaceSvgIds(body);
+
+    const attributes =
+      svgTag.match(/\w+=["'][\w\s.]+["']/g)?.reduce((obj, item) => {
+        const [key, value] = item.split("=");
+
+        return {
+          ...obj,
+          [key.trim()]: value.replace(/["']/g, "").trim(),
+        };
+      }, {}) || {};
+
+    return {
+      body,
+      attributes,
+    };
+  } catch (error: unknown) {
+    console.warn(error);
+
+    return undefined;
+  }
+};
+
+export default getSvgData;
