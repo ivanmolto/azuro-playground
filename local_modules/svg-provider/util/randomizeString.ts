65c3070e54137594e7988ada590bacb5cb6160e5
Ivan Molto <ivanmolto@gmail.com>
2025-11-01 19:25:40 +0100

    feat: Add randomizeString utility function for generating random strings

---
+++ local_modules/svg-provider/util/randomizeString.ts
@@ -0,0 +1,18 @@
+const randomizeString = (length = 8) => {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "1234567890";
+  const charset = letters + letters.toUpperCase() + numbers;
+
+  const randomCharacter = (array: string) =>
+    array[Math.floor(Math.random() * array.length)];
+
+  let R = "";
+
+  for (let i = 0; i < length; i++) {
+    R += randomCharacter(charset);
+  }
+
+  return R;
+};
+
+export default randomizeString;
